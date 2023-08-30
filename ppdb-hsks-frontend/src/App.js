8ac41de1ea1b0fce35794da3fd7064cc77149f87
Adrian nathanael <115018250+AdrianNael@users.noreply.github.com>
2023-08-30 13:11:03 +0700

    Update

--- ppdb-hsks-frontend/src/App.js
+++ ppdb-hsks-frontend/src/App.js
@@ -3,30 +3,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import PPDBForm from './Components/PPDBForm';
 import Kontak from './Components/Pages/Kontak';
+import Home from './Components/Pages/Home';
+import Header from './Components/Organisms/Header';
+import About from './Components/Pages/About'
 
 function App() {
   return (
     <Router>
       <div>
-        <nav className="p-4 bg-gray-900 text-white">
-          <ul className="flex">
-            <li className="mr-6">
-              <Link to="/">Beranda</Link>
-            </li>
-            <li>
-              <Link to="/kontak">Kontak</Link>
-            </li>
-          </ul>
-        </nav>
+        <Header />
         <Switch>
-          <Route path="/kontak">
-            <Kontak />
-          </Route>
-          <Route path="/">
-            <div className="flex justify-center items-center h-screen bg-gray-100">
-              <PPDBForm />
-            </div>
-          </Route>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Kontak} />
         </Switch>
       </div>
     </Router>
--- ppdb-hsks-frontend/tailwind.config.js
+++ ppdb-hsks-frontend/tailwind.config.js
@@ -1,10 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+    "./src/**/*.{js,jsx,ts,tsx}",  ],
   theme: {
-    extend: {},
+    screen: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+  },
+
+    fontFamily: {
+      poppins: ["Poppins"],
+  },
+  extend: {
+      colors: {
+          biruprimary: "#204397",
+          BiruSecondary1: "#27B6C1",
+          BiruSecondary2: "#131E3C",
+          Hijau: "#25D366",
+          Emas: "#B8A200",
+          Kuning: "#FBC72D",
+          AbuAbu: "#9EA9C6",
+      },
+      screens: {
+        'mysm': '300px',
+        'mymd': '640px',
+    }
+  },
   },
   plugins: [],
 }
