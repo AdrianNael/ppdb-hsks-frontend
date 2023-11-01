072fe9b505cbc49d54c5d73dd43f6721d42efeb2
Adrian nathanael <115018250+AdrianNael@users.noreply.github.com>
2023-11-01 00:49:40 -0700

    Update button jenjang lagi

--- src/Components/Atoms/ButtonJenjang.js
+++ src/Components/Atoms/ButtonJenjang.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
-const ButtonJenjang = ({ selectedId, setJenjang }) => {
+function ButtonJenjang ({ selectedId,props }) {
+
   const [id, setId] = useState(selectedId);
   const [isLoading, setIsLoading] = useState(false);
+  const [apiData, setApiData] = useState(null);
 
+  // Fungsi untuk mengambil data dari API
   const fetchDataFromAPI = () => {
     setIsLoading(true);
-    get(variabel unitsekolah)
-
-    // Gantilah URL di bawah dengan URL API yang sesuai
-    fetch(`https://example.com/api/id/${id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setId(data.id);
+    Axios.get("URL_API_ANDA") // Ganti dengan URL API yang sesuai
+      .then((res) => {
+        setApiData(res.data);
         setIsLoading(false);
       })
       .catch((error) => {
-        console.error("Gagal mengambil data:", error);
+        console.error("Error fetching data from API:", error);
         setIsLoading(false);
       });
   };
@@ -26,55 +26,53 @@ const ButtonJenjang = ({ selectedId, setJenjang }) => {
   };
 
   useEffect(() => {
-    fetchDataFromAPI();
-  }, []);
-
-  useEffect(() => {
     // Saat selectedId berubah, perbarui state id
     setId(selectedId);
   }, [selectedId]);
 
   return (
     <>
-    <div className="grid grid-cols-2 gap-8 xl:flex text-white">
-    <button
-        id="TK"
-        onClick={handleJenjangClick}
-        disabled={isLoading}
-        className={`rounded px-4 py-2 text-lg font-medium bg-TK border-2 hover:-skew-y-6 hover:scale-125 hover:transition hover:duration-300 hover:ease-in-out md:font-bold md:text-2xl md:w-40 md:h-20`}
-        data-te-triple-inti
-        data-te-triple-color="light"
-      >
-        TK
-      </button>
-      <button
-        id="SD"
-        onClick={handleJenjangClick}
-        className={`rounded border-2 px-4 py-2 text-lg font-medium bg-SD hover:-skew-y-6 hover:scale-125 hover:transition hover:duration-300 hover:ease-in-out md:font-bold md:text-xl md:w-40 md:h-20`}
-        data-te-triple-inti
-        data-te-triple-color="light"
-      >
-        SD
-      </button>
-      <button
-        id="SMP"
-        onClick={handleJenjangClick}
-        className={`rounded border-2 px-4 py-2 text-lg font-medium bg-SMP hover:-skew-y-6 hover:scale-125 hover:transition hover:duration-300 hover:ease-in-out md:font-bold md:text-xl md:w-40 md:h-20`}
-        data-te-triple-inti
-        data-te-triple-color="light"
-      >
-        SMP
-      </button>
-      <button
-        id="SMA"
-        onClick={handleJenjangClick}
-        className={`rounded border-2 px-4 py-2 text-lg font-medium bg-SMA hover:-skew-y-6 hover:scale-125 hover:transition hover:duration-300 hover:ease-in-out md:font-bold md:text-xl md:w-40 md:h-20`}
-        data-te-triple-inti
-        data-te-triple-color="light"
-      >
-        SMA
-      </button>
-      {isLoading ? <p>Sedang memuat...</p> : <p>ID dari API: {id}</p>}
+      <div className="grid grid-cols-2 gap-8 xl:flex text-white">
+        <button
+          id="TK"
+          onClick={handleJenjangClick}
+          disabled={isLoading}
+          className={`rounded px-4 py-2 text-lg font-medium bg-TK border-2 hover:-skew-y-6 hover:scale-125 hover:transition hover:duration-300 hover:ease-in-out md:font-bold md:text-2xl md:w-40 md:h-20`}
+          data-te-triple-inti
+          data-te-triple-color="light"
+        >
+          TK
+        </button>
+        <button
+          id="SD"
+          onClick={handleJenjangClick}
+          disabled={isLoading}
+          className={`rounded border-2 px-4 py-2 text-lg font-medium bg-SD hover:-skew-y-6 hover:scale-125 hover:transition hover:duration-300 hover:ease-in-out md:font-bold md:text-xl md:w-40 md:h-20`}
+          data-te-triple-inti
+          data-te-triple-color="light"
+        >
+          SD
+        </button>
+        <button
+          id="SMP"
+          onClick={handleJenjangClick}
+          disabled={isLoading}
+          className={`rounded border-2 px-4 py-2 text-lg font-medium bg-SMP hover:-skew-y-6 hover:scale-125 hover:transition hover:duration-300 hover:ease-in-out md:font-bold md:text-xl md:w-40 md:h-20`}
+          data-te-triple-inti
+          data-te-triple-color="light"
+        >
+          SMP
+        </button>
+        <button
+          id="SMA"
+          onClick={handleJenjangClick}
+          disabled={isLoading}
+          className={`rounded border-2 px-4 py-2 text-lg font-medium bg-SMA hover:-skew-y-6 hover:scale-125 hover:transition hover:duration-300 hover:ease-in-out md:font-bold md:text-xl md:w-40 md:h-20`}
+          data-te-triple-inti
+          data-te-triple-color="light"
+        >
+          SMA
+        </button>
       </div>
     </>
   );
